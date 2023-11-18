d2d068c59e6dd2c4f5b671d5128769f4dcd0140d
Restyled.io <commits@restyled.io>
2023-11-18 19:45:35 +0000

    Restyled by prettier

--- client/app/main/page.js
+++ client/app/main/page.js
@@ -1,8 +1,8 @@
-'use client';
-import TopBar from '../topbar';
-import Chat from './chat/chat';
-import Renderer from './renderer/renderer';
-import Editor from './editor/editor';
+"use client";
+import TopBar from "../topbar";
+import Chat from "./chat/chat";
+import Renderer from "./renderer/renderer";
+import Editor from "./editor/editor";
 import {
   Box,
   Button,
@@ -10,20 +10,20 @@ import {
   InputLabel,
   Select,
   MenuItem,
-  Stack
-} from '@mui/material';
-import {useState} from 'react';
+  Stack,
+} from "@mui/material";
+import { useState } from "react";
 
 export default function Page() {
   const [showChat, setShowChat] = useState(true);
-  const [html, setHtml] = useState('<h1> Renderer </h1>');
-  const [css, setCss] = useState('');
-  const [js, setJs] = useState('');
-  const [framework, setFramework] = useState('html');
+  const [html, setHtml] = useState("<h1> Renderer </h1>");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+  const [framework, setFramework] = useState("html");
 
   const handleChange = (event) => {
     setFramework(event.target.value);
-  }
+  };
 
   return (
     <div>
@@ -33,27 +33,30 @@ export default function Page() {
         <FormControl fullWidth>
           <InputLabel id="demo-simple-select-label">Framework</InputLabel>
           <Select
-  labelId = "demo-simple-select-label"
-  id = "demo-simple-select"
-  value = {framework} label = "Framework"
-  onChange = {handleChange} sx =
-      {{ width: '150px' }} >
-      <MenuItem value = {"html"}>
-          Html</MenuItem>
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={framework}
+            label="Framework"
+            onChange={handleChange}
+            sx={{ width: "150px" }}
+          >
+            <MenuItem value={"html"}>Html</MenuItem>
             <MenuItem value={"react"}>React</MenuItem>
-      <MenuItem value = {"vue"}>Vue<
-          /MenuItem>
+            <MenuItem value={"vue"}>Vue</MenuItem>
             <MenuItem value={"angular"}>Angular</MenuItem>
-      <MenuItem value = {"svelte"}>Svelte</MenuItem>
+            <MenuItem value={"svelte"}>Svelte</MenuItem>
           </Select>
-      </FormControl>
+        </FormControl>
       </Stack>
 
-      < Box
-  key = 'main'
-  display = 'flex'
-  flexDirection = 'row'
-  justifyContent = 'center' > {showChat ? (
+      <Box
+        key="main"
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+      >
+        {" "}
+        {showChat ? (
           <Chat />
         ) : (
           <Editor
@@ -62,12 +65,11 @@ export default function Page() {
             js={js}
             setHtml={setHtml}
             setCss={setCss}
-            setJs={
-      setJs}
+            setJs={setJs}
           />
         )}
         <Renderer html={html} css={css} js={js} />
       </Box>
     </div>
   );
-  }
+}
