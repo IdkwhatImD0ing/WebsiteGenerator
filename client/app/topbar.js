f191ae897c1abcca757de67c5434027cd99a59c1
Restyled.io <commits@restyled.io>
2023-11-18 23:11:13 +0000

    Restyled by prettier

--- client/app/topbar.js
+++ client/app/topbar.js
@@ -1,9 +1,9 @@
-import {UserButton} from '@clerk/nextjs';
-import {Box} from '@mui/material';
+import { UserButton } from "@clerk/nextjs";
+import { Box } from "@mui/material";
 export default function TopBar() {
   return (
     <Box>
-      <UserButton afterSignOutUrl='/' />
+      <UserButton afterSignOutUrl="/" />
     </Box>
   );
 }
