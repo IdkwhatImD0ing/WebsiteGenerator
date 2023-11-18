a57ae006c66260fff4a7e547bd524bcc1b672d94
Restyled.io <commits@restyled.io>
2023-11-18 22:22:16 +0000

    Restyled by prettier

--- client/app/main/chat.js
+++ client/app/main/chat.js
@@ -1,41 +1,43 @@
-'use client';
-import {Box, Paper} from '@mui/material';
-import {useState, useEffect, useRef} from 'react';
-import ChatMessageBox from './chatMessageBox';
-import TextInput from './textInput';
-import axios from 'axios';
+"use client";
+import { Box, Paper } from "@mui/material";
+import { useState, useEffect, useRef } from "react";
+import ChatMessageBox from "./chatMessageBox";
+import TextInput from "./textInput";
+import axios from "axios";
 
-export default function Chat({messages, setMessages, html, setHtml}) {
-  const addMessage = async newMessage => {
+export default function Chat({ messages, setMessages, html, setHtml }) {
+  const addMessage = async (newMessage) => {
     const messageToChat = newMessage + `\nThe current html is \n${html}`;
     console.log(messageToChat);
-    setMessages([...messages, {content : newMessage, role : 'user'} ]);
-    const newMessagesToChat =
-        [...messages, {content : messageToChat, role : 'user'} ];
-    const conversation = {type : 'text', messages : newMessagesToChat};
-    const response =
-        await axios.post('http://localhost:8000/chat', conversation,
-                         {headers : {'Content-Type' : 'application/json'}});
+    setMessages([...messages, { content: newMessage, role: "user" }]);
+    const newMessagesToChat = [
+      ...messages,
+      { content: messageToChat, role: "user" },
+    ];
+    const conversation = { type: "text", messages: newMessagesToChat };
+    const response = await axios.post(
+      "http://localhost:8000/chat",
+      conversation,
+      { headers: { "Content-Type": "application/json" } },
+    );
     console.log(response);
     // TODO: check for markdown
     setHtml(response.data.slice(7, -3));
   };
   return (
     <Box
-  key = 'chat'
-  backgroundColor = 'white'
-  color = 'black'
-  width = '100%'
-  flexGrow = '1'
-  display = 'flex'
-      flexDirection='column'
+      key="chat"
+      backgroundColor="white"
+      color="black"
+      width="100%"
+      flexGrow="1"
+      display="flex"
+      flexDirection="column"
     >
-      <Box flexGrow='1' overflow='auto'>
-        <ChatMessageBox messages={
-    messages} />
+      <Box flexGrow="1" overflow="auto">
+        <ChatMessageBox messages={messages} />
       </Box>
-      <TextInput addMessage={
-    addMessage} />
+      <TextInput addMessage={addMessage} />
     </Box>
   );
 }
--- client/app/main/editor.js
+++ client/app/main/editor.js
@@ -1,42 +1,45 @@
-'use client';
-import {Box, TextField, Button} from '@mui/material';
-import AceEditor from 'react-ace';
-import {useState} from 'react';
-export default function Editor({html, setHtml}) {
+"use client";
+import { Box, TextField, Button } from "@mui/material";
+import AceEditor from "react-ace";
+import { useState } from "react";
+export default function Editor({ html, setHtml }) {
   const [htmlText, setHtmlText] = useState(html);
 
-  const handleChange = (newValue, setter) => { setter(newValue); };
+  const handleChange = (newValue, setter) => {
+    setter(newValue);
+  };
 
-  const refresh = () => { setHtml(htmlText); };
+  const refresh = () => {
+    setHtml(htmlText);
+  };
 
   return (
     <Box
-  key = 'editor'
-  backgroundColor = 'white'
-  color = 'black'
-  width = '100%'
-  flexGrow = '1'
-  display = 'flex'
-  flexDirection =
-      'column' >
-      <Button onClick = {refresh}>Refresh<
-          /Button>
+      key="editor"
+      backgroundColor="white"
+      color="black"
+      width="100%"
+      flexGrow="1"
+      display="flex"
+      flexDirection="column"
+    >
+      <Button onClick={refresh}>Refresh</Button>
       <AceEditor
-        mode='html'
-        theme='monokai'
+        mode="html"
+        theme="monokai"
         value={htmlText}
-        onChange={e => handleChange(e, setHtmlText)}
-        name='html_editor'
+        onChange={(e) => handleChange(e, setHtmlText)}
+        name="html_editor"
         editorProps={{ $blockScrolling: true }}
         setOptions={{
           enableBasicAutocompletion: true,
           enableLiveAutocompletion: true,
           enableSnippets: true,
           showLineNumbers: true,
-          tabSize: 2
+          tabSize: 2,
         }}
-        style={{ width: '100%', height: '100%' }}
-      /><
-      /Box>
+        style={{ width: "100%", height: "100%" }}
+      />
+    </Box>
   );
 }
