8d96a8e9f3e37b583f70b1e51d37cf9d60cbae65
Hebe3834 <hebeluna00@gmail.com>
2023-11-18 17:13:00 -0500

    styling and theme attempt

--- client/app/sign-in/page.js
+++ client/app/sign-in/page.js
@@ -2,6 +2,7 @@
 import { SignIn } from '@clerk/nextjs';
 import { Box, CircularProgress } from '@mui/material';
 import { useAuth } from '@clerk/nextjs';
+import Image from 'next/image';
 
 export default function Page() {
   const { isLoaded } = useAuth();
@@ -23,16 +24,28 @@ export default function Page() {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.5 }}
-          width='70%'
-          height='80%'
-          padding={4}
+          width='100vw'
+          height='100vh'
+          padding={0}
+          margin={0}
           backgroundColor='white'
           display='flex'
           flexDirection='row'
-          justifyContent='center'
+          justifyContent='space-between'
           alignItems='center'
         >
-          <SignIn />
+          <div style={{width: '50%', height: '100%', position: 'relative'}}>
+            <Image
+              priority={true}
+              src="/spiderweb.png"
+              alt="spiderweb"
+              layout='fill'
+              objectFit='cover'
+            />
+          </div>
+          <div style={{width: '50%', height: '100%', position: 'relative', display: 'flex', alignItems: "center", justifyContent: "center"}}>
+            <SignIn />
+          </div>
         </Box>
       )}
     </Box>
--- client/app/sign-up/page.js
+++ client/app/sign-up/page.js
@@ -2,9 +2,16 @@
 import { SignUp } from '@clerk/nextjs';
 import { Box, CircularProgress } from '@mui/material';
 import { useAuth } from '@clerk/nextjs';
+import Image from 'next/image';
+import Stack from '@mui/material/Stack';
+import { useTheme } from '@mui/material/styles';
+import theme from '../theme';
 
 export default function Page() {
   const { isLoaded } = useAuth();
+  // const theme = useTheme();
+  console.log(theme);
+
   return (
     <Box
       key='signup'
@@ -14,26 +21,40 @@ export default function Page() {
       flexDirection='row'
       justifyContent='center'
       alignItems='center'
+      backgroundColor="primary"
     >
       {!isLoaded ? (
         <CircularProgress />
       ) : (
-        <Box
+        <Stack
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.5 }}
-          width='70%'
-          height='80%'
-          padding={4}
-          backgroundColor='white'
-          display='flex'
-          flexDirection='row'
-          justifyContent='center'
+          width='100vw'
+          height='100vh'
+          padding={0}
+          margin={0}
+          backgroundColor="primary"
+          direction='row'
+          justifyContent='space-between'
           alignItems='center'
         >
-          <SignUp />
-        </Box>
+            <div style={{width: '50%', height: '100%', position: 'relative'}}>
+              <Image
+                priority={true}
+                src="/spiderweb.png"
+                alt="spiderweb"
+                layout='fill'
+                objectFit='cover'
+              />
+            </div>
+            <Box backgroundColor='primary'>
+              <div style={{width: '50%', height: '100%', position: 'relative', display: 'flex', alignItems: "center", justifyContent: "center"}}>
+                <SignUp />
+              </div>
+            </Box>
+        </Stack>
       )}
     </Box>
   );
