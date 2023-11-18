f6cfa5d7b1cbbda428a4906ae4b146734830b7fb
Restyled.io <commits@restyled.io>
2023-11-18 19:08:03 +0000

    Restyled by black

--- server/main.py
+++ server/main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
 from dotenv import load_dotenv
+
 load_dotenv()
 
 GENERATION_TIMEOUT_SEC = 60
@@ -26,12 +27,13 @@ app.add_middleware(
 @app.post("/chat")
 async def text_chat(conversation: Conversation):
     # Message conversion:
-    for i in range(len(conversation.messages)-1):
+    for i in range(len(conversation.messages) - 1):
         # Convert all image messages to messages except the last one
         msg = conversation.messages[i]
         if isinstance(msg, ImageMessage):
             conversation.messages[i] = Message(
-                role=msg.role, content=msg.content[0].text)
+                role=msg.role, content=msg.content[0].text
+            )
 
     systemMessage = Message(
         role="system",
@@ -39,7 +41,7 @@ async def text_chat(conversation: Conversation):
 You are a web development agent specialized in interpreting user inputs to create HTML pages styled with TailwindCSS. 
 Your task is to take user descriptions of their desired webpage and convert these descriptions into valid, renderable HTML code using TailwindCSS classes. You should not provide guidance, examples, or suggestions - focus solely on generating the HTML code based on the input given. 
 Ensure that all generated HTML is valid and can be rendered correctly with TailwindCSS.
-"""
+""",
     )
 
     # Insert into beginning of conversation
@@ -53,7 +55,9 @@ Ensure that all generated HTML is valid and can be rendered correctly with Tailw
             temperature=0,
         )
 
-        return JSONResponse(content=response.choices[0].message.content, status_code=200)
+        return JSONResponse(
+            content=response.choices[0].message.content, status_code=200
+        )
     else:
         response = await client.chat.completions.create(
             model="gpt-4-vision-preview",
@@ -62,4 +66,6 @@ Ensure that all generated HTML is valid and can be rendered correctly with Tailw
             temperature=0,
         )
 
-        return JSONResponse(content=response.choices[0].message.content, status_code=200)
+        return JSONResponse(
+            content=response.choices[0].message.content, status_code=200
+        )
