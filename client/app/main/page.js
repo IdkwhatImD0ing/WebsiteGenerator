282ddeb126a5fad44aa500fcc1615c0a6ae4eb15
Edward Chen <echen333us@gmail.com>
2023-11-18 14:26:24 -0500

    languages

--- client/app/main/page.js
+++ client/app/main/page.js
@@ -3,7 +3,7 @@ import TopBar from '../topbar';
 import Chat from './chat/chat';
 import Renderer from './renderer/renderer';
 import Editor from './editor/editor';
-import { Box, Button } from '@mui/material';
+import { Box, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { useState } from 'react';
 
 export default function Page() {
@@ -11,10 +11,40 @@ export default function Page() {
   const [html, setHtml] = useState('<h1> Renderer </h1>');
   const [css, setCss] = useState('');
   const [js, setJs] = useState('');
+  const [language, setLanguage] = useState('html');
+
+  const handleChange = (event) => {
+    setLanguage(event.target.value);
+  }
+
   return (
     <div>
       <TopBar />
-      <Button onClick={() => setShowChat(!showChat)}>Toggle</Button>
+      {/* # display inline div, have some space between them */}
+      <div style={{display: 'inline-flex', justifyContent: 'space-between', width: '100%'}}>
+        <Button onClick={() => setShowChat(!showChat)}>Toggle</Button>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={language}
+            label="Language"
+            onChange={handleChange}
+            sx={{width: '150px'}}
+          >
+            <MenuItem value={"html"}>Html</MenuItem>
+            <MenuItem value={"react"}>React</MenuItem>
+            <MenuItem value={"vue"}>Vue</MenuItem>
+            <MenuItem value={"angular"}>Angular</MenuItem>
+            <MenuItem value={"svelte"}>Svelte</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <h2>
+        {language}
+      </h2>
+
       <Box
         key='main'
         display='flex'
--- client/app/page.js
+++ client/app/page.js
@@ -2,7 +2,10 @@ import Image from 'next/image';
 import TopBar from './topbar';
 export default function Home() {
   return (
-    <div>
+    <div style={{height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
+    
+    }}>
+      {/* main */}
       <TopBar />
       <h1>Home Page</h1>
     </div>
