17708ea3dda3aab03fa5b7dcb820cef57a78212d
Restyled.io <commits@restyled.io>
2023-11-18 20:55:13 +0000

    Restyled by prettier

--- client/lib/redis.js
+++ client/lib/redis.js
@@ -1,16 +1,15 @@
-import {createClient} from 'redis'
+import { createClient } from "redis";
 
 const client = createClient({
-  password : process.env.REDIS_PASSWORD,
-  socket : {
-    host : process.env.REDIS_HOST,
-    port : process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
   },
-})
+});
 
-client
-    .on('error', (err) => console.log('Redis Client Error', err))
+client.on("error", (err) => console.log("Redis Client Error", err));
 
-        await client.connect()
+await client.connect();
 
-export default client
+export default client;
