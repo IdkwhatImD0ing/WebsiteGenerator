cbb9990d9eff590856c40e8639bb0c1bd4f9f37a
Restyled.io <commits@restyled.io>
2023-11-18 22:25:54 +0000

    Restyled by clang-format

--- client/app/theme.js
+++ client/app/theme.js
@@ -1,33 +1,32 @@
 'use client'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import{createTheme, ThemeProvider} from '@mui/material/styles';
 
-const customTheme = (isDarkMode) => 
-createTheme({
-    palette: {
-        primary: {
-            main: "#28282B",
-        }
-    },
-    blue: {
-        primary: {
-            main: "#5D3FD3",
-        }
-    },
-    green: {
-        primary: {
-            main: "#3FD3A7",
-        }
-    },
-    yellow: {
-        primary: {
-            main: "#B5D33F",
-        }
-    },
-    red: {
-        primary: {
-            main: "#D33F6B",
-        }
+const customTheme = (isDarkMode) => createTheme({
+  palette : {
+    primary : {
+      main : "#28282B",
     }
+  },
+  blue : {
+    primary : {
+      main : "#5D3FD3",
+    }
+  },
+  green : {
+    primary : {
+      main : "#3FD3A7",
+    }
+  },
+  yellow : {
+    primary : {
+      main : "#B5D33F",
+    }
+  },
+  red : {
+    primary : {
+      main : "#D33F6B",
+    }
+  }
 });
 
 export default function CustomTheme() {
