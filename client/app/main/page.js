7d4e2c74ae48e13a9f3af201707851d1bcb6c241
Restyled.io <commits@restyled.io>
2023-11-18 22:11:43 +0000

    Restyled by prettier

--- client/app/main/page.js
+++ client/app/main/page.js
@@ -1,9 +1,9 @@
-'use client';
-import TopBar from '../topbar';
-import Chat from './chat';
-import Renderer from './renderer';
-import Editor from './editor';
-import Sidebar from './sidebar';
+"use client";
+import TopBar from "../topbar";
+import Chat from "./chat";
+import Renderer from "./renderer";
+import Editor from "./editor";
+import Sidebar from "./sidebar";
 import {
   Box,
   Button,
@@ -11,56 +11,66 @@ import {
   InputLabel,
   Select,
   MenuItem,
-  Stack
-} from '@mui/material';
-import {useState} from 'react';
+  Stack,
+} from "@mui/material";
+import { useState } from "react";
 
 export default function Page() {
   const [showChat, setShowChat] = useState(true);
-  const [html, setHtml] = useState('<h1> Renderer </h1>');
-  const [css, setCss] = useState('');
-  const [js, setJs] = useState('');
-  const [framework, setFramework] = useState('html');
+  const [html, setHtml] = useState("<h1> Renderer </h1>");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+  const [framework, setFramework] = useState("html");
   const [messages, setMessages] = useState([]);
-  const handleChange = event => { setFramework(event.target.value); };
+  const handleChange = (event) => {
+    setFramework(event.target.value);
+  };
 
   return (
     <div>
       <TopBar />
-      <Stack direction='row' spacing={2} justifyContent="space-between" alignItems="center">
-        <Sidebar/>
+      <Stack
+        direction="row"
+        spacing={2}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Sidebar />
         <Button onClick={() => setShowChat(!showChat)}>Toggle</Button>
         <FormControl fullWidth>
-          <InputLabel id='demo-simple-select-label'>Framework</InputLabel>
+          <InputLabel id="demo-simple-select-label">Framework</InputLabel>
           <Select
-  labelId = 'demo-simple-select-label'
-  id = 'demo-simple-select'
-  value = {framework} label = 'Framework'
-  onChange = {handleChange} sx =
-      {{ width: '150px' }} >
-      <MenuItem value = {'html'}>
-          Html</MenuItem>
-            <MenuItem value={'react'}>React</MenuItem>
-      <MenuItem value = {'vue'}>Vue<
-          /MenuItem>
-            <MenuItem value={'angular'}>Angular</MenuItem>
-      <MenuItem value = {'svelte'}>Svelte</MenuItem>
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={framework}
+            label="Framework"
+            onChange={handleChange}
+            sx={{ width: "150px" }}
+          >
+            <MenuItem value={"html"}>Html</MenuItem>
+            <MenuItem value={"react"}>React</MenuItem>
+            <MenuItem value={"vue"}>Vue</MenuItem>
+            <MenuItem value={"angular"}>Angular</MenuItem>
+            <MenuItem value={"svelte"}>Svelte</MenuItem>
           </Select>
-      </FormControl>
+        </FormControl>
       </Stack>
 
-      < Box
-  key = 'main'
-  display = 'flex'
-  flexDirection = 'row'
-  justifyContent = 'center' > {' '} {showChat ? (< Chat
+      <Box
+        key="main"
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+      >
+        {" "}
+        {showChat ? (
+          <Chat
             messages={messages}
             html={html}
             setMessages={setMessages}
             setHtml={setHtml}
             setCss={setCss}
-            setJs={
-    setJs}
+            setJs={setJs}
           />
         ) : (
           <Editor
@@ -72,8 +82,7 @@ export default function Page() {
             setJs={setJs}
           />
         )}
-        <Renderer html={html} css={css} js={
-    js} />
+        <Renderer html={html} css={css} js={js} />
       </Box>
     </div>
   );
