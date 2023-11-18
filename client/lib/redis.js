e2a0663b229610192524ece02816038e46a1c748
Bill Zhang <billzhangsc@gmail.com>
2023-11-18 12:54:50 -0800

    Redis

---
+++ client/lib/redis.js
@@ -0,0 +1,15 @@
+import {createClient} from 'redis'
+
+const client = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+})
+
+client.on('error', (err) => console.log('Redis Client Error', err))
+
+await client.connect()
+
+export default client
