5a5128c2f16c91e9b589d792b2afa9e35c346f31
Restyled.io <commits@restyled.io>
2023-11-19 04:08:54 +0000

    Restyled by prettier

--- client/app/main/page.js
+++ client/app/main/page.js
@@ -4,7 +4,7 @@ import Chat from "./chat";
 import Renderer from "./renderer";
 import Editor from "./editor";
 import Sidebar from "./sidebar";
-import {useTheme} from '@mui/material/styles';
+import { useTheme } from "@mui/material/styles";
 
 import {
   Box,
@@ -15,7 +15,7 @@ import {
   MenuItem,
   Stack,
 } from "@mui/material";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Page() {
   const theme = useTheme();
@@ -25,18 +25,23 @@ export default function Page() {
   const [framework, setFramework] = useState("html");
 
   const [messages, setMessages] = useState([]);
-  const handleChange = (event) => { setFramework(event.target.value); };
+  const handleChange = (event) => {
+    setFramework(event.target.value);
+  };
 
   return (
     <div>
       <TopBar />
       <Stack
-  direction = "row"
-  spacing = {2} justifyContent = "space-between"
-  alignItems =
-      "center" > <Sidebar /><FormControl fullWidth>
-      <InputLabel id = "demo-simple-select-label">Framework<
-          /InputLabel>
+        direction="row"
+        spacing={2}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        {" "}
+        <Sidebar />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Framework</InputLabel>
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
@@ -46,13 +51,11 @@ export default function Page() {
             sx={{ width: "150px" }}
           >
             <MenuItem value={"html"}>Html</MenuItem>
-      <MenuItem value = {"react"}>
-          React</MenuItem>
+            <MenuItem value={"react"}>React</MenuItem>
             <MenuItem value={"vue"}>Vue</MenuItem>
-      <MenuItem value = {"angular"}>Angular<
-          /MenuItem>
+            <MenuItem value={"angular"}>Angular</MenuItem>
             <MenuItem value={"svelte"}>Svelte</MenuItem>
-      </Select>
+          </Select>
         </FormControl>
       </Stack>
 
@@ -64,36 +67,48 @@ export default function Page() {
       >
         {" "}
         <Box width="45vw" height="80vh" display="flex" flexDirection="column">
-          <Box width="100%" height="7vh" display="flex" flexDirection="row" justifyContent="space-between">
+          <Box
+            width="100%"
+            height="7vh"
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+          >
             <Button
               variant="contained"
               sx={{
-                backgroundColor: showChat ? "background.green" : "background.inverted",
-                "&:hover": {backgroundColor: "background.green"}
+                backgroundColor: showChat
+                  ? "background.green"
+                  : "background.inverted",
+                "&:hover": { backgroundColor: "background.green" },
               }}
               height="10px"
               fullWidth={true}
               onClick={() => setShowChat(true)}
             >
               Chat
-            </Button><
-      Button
-  variant = "contained"
-  sx = {
-    {
-      backgroundColor: showChat ? "background.inverted" : "background.red",
-          "&:hover": {backgroundColor: "background.red"},
-    }
-  } height = "10px"
-  fullWidth = {true} onClick = {() => setShowChat(false)} >
-                               Code Editor</Button>
-          </Box>{showChat
-                                                                     ? (< Chat
+            </Button>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: showChat
+                  ? "background.inverted"
+                  : "background.red",
+                "&:hover": { backgroundColor: "background.red" },
+              }}
+              height="10px"
+              fullWidth={true}
+              onClick={() => setShowChat(false)}
+            >
+              Code Editor
+            </Button>
+          </Box>
+          {showChat ? (
+            <Chat
               messages={messages}
               html={html}
               setMessages={setMessages}
-              setHtml={
-    setHtml}
+              setHtml={setHtml}
             />
           ) : (
             <Editor html={html} setHtml={setHtml} />
--- client/app/theme.js
+++ client/app/theme.js
@@ -1,38 +1,35 @@
-'use client'
-import{createTheme, ThemeProvider} from '@mui/material/styles';
+"use client";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const customTheme = (isDarkMode) => createTheme({
-  palette : {
-    mode : isDarkMode ? 'dark' : 'light',
-    primary : {
-      main : "#28282B",
-      blue : "#5D3FD3",
-      green : "#3FD3A7",
-      yellow : "#B5D33F",
-      red : "#D33F6B"
+const customTheme = (isDarkMode) =>
+  createTheme({
+    palette: {
+      mode: isDarkMode ? "dark" : "light",
+      primary: {
+        main: "#28282B",
+        blue: "#5D3FD3",
+        green: "#3FD3A7",
+        yellow: "#B5D33F",
+        red: "#D33F6B",
+      },
+      background: {
+        default: isDarkMode ? "#121212" : "#28282B",
+        inverted: "#28282B",
+        blue: "#5D3FD3",
+        green: "#3FD3A7",
+        yellow: "#B5D33F",
+        red: "#D33F6B",
+      },
+      text: {
+        primary: isDarkMode ? "#ffffff" : "#000000",
+        secondary: isDarkMode ? "#a0a0a0" : "#303030",
+      },
+      typography: {
+        fontFamily: "'Roboto', sans-serif",
+      },
     },
-    background : {
-      default : isDarkMode ? '#121212' : '#28282B',
-      inverted : '#28282B',
-      blue : "#5D3FD3",
-      green : "#3FD3A7",
-      yellow : "#B5D33F",
-      red : "#D33F6B"
-    },
-    text : {
-      primary : isDarkMode ? '#ffffff' : '#000000',
-      secondary : isDarkMode ? '#a0a0a0' : '#303030',
-    },
-    typography : {
-      fontFamily : "'Roboto', sans-serif",
-    },
-  }
-});
+  });
 
-export default function CustomTheme({children}) {
-    return (
-        <ThemeProvider theme={customTheme(false)}>
-            {children}
-        </ThemeProvider>
-    );
-};
+export default function CustomTheme({ children }) {
+  return <ThemeProvider theme={customTheme(false)}>{children}</ThemeProvider>;
+}
