50c224b9b80f61cff24514f59063e26a10813e37
Restyled.io <commits@restyled.io>
2023-11-19 10:51:44 +0000

    Restyled by clang-format

--- client/app/billing/page.js
+++ client/app/billing/page.js
@@ -1,54 +1,49 @@
 "use client";
 import TopBar from "../topbar";
-import { Box } from "@mui/material";
-import { Typography } from '@mui/material';
-
+import {Box} from "@mui/material";
+import {Typography} from '@mui/material';
 
 export default function Page() {
-    return (
-        <div>
-            <TopBar />
-            <Box width="100vw" height="100vh" display="flex" flexDirection="column" alignItems="center">
-                <Typography variant="h2" margin={5}>
-                    Find a plan that's right for you
-                </Typography>
+  return (
+      <div><TopBar /><Box width = "100vw" height = "100vh" display =
+                          "flex" flexDirection = "column" alignItems = "center">
+      <Typography variant = "h2" margin = {5}>Find a plan that's right for you <
+          /Typography>
 
                 <Box width="90%" display="flex" flexDirection="row" justifyContent="space-around">
                     <Box width={300} height={500} display="flex" flexDirection="column" border="2px solid black">
                         <Typography variant="h4" margin={5}>
                             FREE
                         </Typography>
-                        <Typography variant="h4" margin={5}>
-                            $0/month
-                        </Typography>
+      <Typography variant = "h4" margin = {5}>$0 /
+      month<
+          /Typography>
                         <Typography variant="h4" margin={5}>
                             1 website
                         </Typography>
-                    </Box>
+      </Box>
                     <Box width={300} height={500} display="flex" flexDirection="column" border="2px solid black">
                         <Typography variant="h4" margin={5}>
                             CASUAL
                         </Typography>
-                        <Typography variant="h4" margin={5}>
-                            $1/month
-                        </Typography>
+      <Typography variant = "h4" margin = {5}>$1 /
+      month<
+          /Typography>
                         <Typography variant="h4" margin={5}>
                             5 website
                         </Typography>
-                    </Box>
+      </Box>
                     <Box width={300} height={500} display="flex" flexDirection="column" border="2px solid black">
                         <Typography variant="h4" margin={5}>
                             PRO
                         </Typography>
-                        <Typography variant="h4" margin={5}>
-                            $5/month
-                        </Typography>
+      <Typography variant = "h4" margin = {5}>$5 /
+      month<
+          /Typography>
                         <Typography variant="h4" margin={5}>
                             Unlimited websites
                         </Typography>
-                    </Box>
-                </Box>
-            </Box>
-        </div>
-    );
+      </Box>
+                </Box></Box>
+        </div>);
 }
