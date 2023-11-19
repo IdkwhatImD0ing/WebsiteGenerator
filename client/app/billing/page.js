a4136e696b87b4a3d61e94d905584f05b9e1b12b
Hebe3834 <hebeluna00@gmail.com>
2023-11-18 21:30:20 -0500

    started billing page

---
+++ client/app/billing/page.js
@@ -0,0 +1,14 @@
+"use client";
+import TopBar from "../topbar";
+import { Box } from "@mui/material";
+
+export default function Page() {
+    return (
+        <div>
+            <TopBar />
+            <Box width="100vw" height="100vh">
+                testing
+            </Box>
+        </div>
+    );
+}
