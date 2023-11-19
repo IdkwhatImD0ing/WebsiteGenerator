9fc69cfb3214779931291ccf538994bbf4413e5a
Hebe3834 <hebeluna00@gmail.com>
2023-11-18 21:11:58 -0500

    updates

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
+import CustomTheme from './theme'
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <CustomTheme>
+          <body className={inter.className}>{children}</body>
+        </CustomTheme>
       </html>
     </ClerkProvider>
   );
