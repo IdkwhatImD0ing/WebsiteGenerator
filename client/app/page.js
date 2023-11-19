60870fb00bea60953b0da7ab1692564211c1467a
Hebe3834 <hebeluna00@gmail.com>
2023-11-19 07:28:33 -0500

    styling

--- client/app/page.js
+++ client/app/page.js
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { Box, Button } from "@mui/material";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { Typography } from "@mui/material";
+import Image from "next/image";
 
 export default function Home() {
   const { userId } = auth();
@@ -20,15 +22,65 @@ export default function Home() {
       sx={{
         backgroundImage: "linear-gradient(to right, #ff6e7f, #bfe9ff)",
       }}
+      position="relative"
     >
-      <h1>Website Generator</h1>
-      <Box display="flex" flexDirection="row">
-        <Link href="/sign-in" passHref>
-          <Button>Sign-in</Button>
-        </Link>
-        <Link href="/sign-up" passHref>
-          <Button>Sign-up</Button>
-        </Link>
+      <Image
+        priority={true}
+        src="/window.png"
+        alt="window "
+        layout="fill"
+        objectFit="cover"
+      />
+      <Box
+        position="absolute"
+        width="100vw"
+        height="100vh"
+        sx={{
+          backgroundImage:
+            "linear-gradient(to bottom, rgba(255, 99, 71, 0.3), rgba(12, 15, 15, 0.5))",
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "0",
+            right: "0",
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+            flexDirection: "row",
+          }}
+        >
+          <Link href="/sign-in" passHref>
+            <Button variant="contained">Sign-in</Button>
+          </Link>
+          <Link href="/sign-up" passHref>
+            <Button variant="contained">Sign-up</Button>
+          </Link>
+        </Box>
+
+        <Box
+          display="flex"
+          flexDirection="column"
+          position="absolute"
+          color="primary.red"
+          alignItems="center"
+        >
+          <Typography
+            variant="h2"
+            margin={5}
+            paddingTop={20}
+            color="primary.blue"
+            fontWeight="bold"
+          >
+            WebWeaver
+          </Typography>
+          <Typography variant="h6" margin={10} color="white">
+            WebWeaver is an AI-powered website generation tool that enables
+            seamless collaboration with an AI chatbox to design, edit, and
+            refine your ideal website in real-time.
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
