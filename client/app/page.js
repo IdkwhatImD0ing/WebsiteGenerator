463702206c4e29c352ef76d44e3ad7ad787fa5df
Hebe3834 <hebeluna00@gmail.com>
2023-11-19 07:50:22 -0500

    fixed typo

--- client/app/page.js
+++ client/app/page.js
@@ -94,7 +94,7 @@ export default function Home() {
             flexDirection: "row",
           }}
         >
-          <Link href="/main" passHref>
+          <Link href="/sign-in" passHref>
             <Button variant="contained">Sign In</Button>
           </Link>
           <Link href="/sign-up" passHref>
