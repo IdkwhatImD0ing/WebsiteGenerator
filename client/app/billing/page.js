8ac4b94cc285d34e77a2d3877e26a9bee81641c9
Hebe3834 <hebeluna00@gmail.com>
2023-11-19 06:01:05 -0500

    billing and readme

--- client/app/billing/page.js
+++ client/app/billing/page.js
@@ -1,12 +1,10 @@
 "use client";
-import TopBar from "../topbar";
 import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
 
 export default function Page() {
   return (
     <div>
-      <TopBar />
       <Box
         width="100vw"
         height="100vh"
