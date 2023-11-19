a724eb000e0fd55e33524936d5dcb63446e3b7aa
Bill Zhang <billzhangsc@gmail.com>
2023-11-19 03:15:34 -0800

    Fix

--- client/app/sign-up/page.js
+++ client/app/sign-up/page.js
@@ -1,22 +1,15 @@
-"use client";
-import { SignUp } from "@clerk/nextjs";
-import { Box, CircularProgress } from "@mui/material";
-import { useAuth } from "@clerk/nextjs";
-import Image from "next/image";
-import Stack from "@mui/material/Stack";
-<<<<<<< HEAD
+'use client'
+import {SignUp} from '@clerk/nextjs'
+import {Box, CircularProgress} from '@mui/material'
+import {useAuth} from '@clerk/nextjs'
+import Image from 'next/image'
+import Stack from '@mui/material/Stack'
+import {useTheme} from '@mui/material/styles'
 
 export default function Page() {
-  const { isLoaded } = useAuth();
-  // const theme = useTheme();
-=======
-import { useTheme } from "@mui/material/styles";
+  const {isLoaded} = useAuth()
+  const theme = useTheme()
 
-export default function Page() {
-  const { isLoaded } = useAuth();
-  const theme = useTheme();
-
->>>>>>> main
   return (
     <Box
       key="signup"
@@ -26,40 +19,29 @@ export default function Page() {
       flexDirection="row"
       justifyContent="center"
       alignItems="center"
-<<<<<<< HEAD
-      backgroundColor="primary.main"
-=======
       backgroundColor="background.inverted"
->>>>>>> main
     >
-      {" "}
+      {' '}
       {!isLoaded ? (
         <CircularProgress />
       ) : (
         <Stack
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.5 }}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          transition={{duration: 0.5}}
           width="100vw"
           height="100vh"
           padding={0}
           margin={0}
-<<<<<<< HEAD
-          backgroundColor="primary"
-=======
           backgroundColor="theme.palette.red.main"
->>>>>>> main
           direction="row"
           justifyContent="space-between"
           alignItems="center"
         >
-<<<<<<< HEAD
-=======
-          {" "}
+          {' '}
           {/* <Box width="50%" height="100%"> */}
->>>>>>> main
-          <div style={{ width: "50%", height: "100%", position: "relative" }}>
+          <div style={{width: '50%', height: '100%', position: 'relative'}}>
             <Image
               priority={true}
               src="/spiderweb.png"
@@ -68,21 +50,6 @@ export default function Page() {
               objectFit="cover"
             />
           </div>
-<<<<<<< HEAD
-          <Box backgroundColor="primary">
-            <div
-              style={{
-                width: "50%",
-                height: "100%",
-                position: "relative",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <SignUp />
-            </div>
-=======
           {/* </Box> */}
           <Box
             backgroundColor="theme.palette.red.main"
@@ -93,10 +60,9 @@ export default function Page() {
             justifyContent="center"
           >
             <SignUp />
->>>>>>> main
           </Box>
         </Stack>
       )}
     </Box>
-  );
+  )
 }
