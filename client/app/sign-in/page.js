ac41f2b2a661c1fc71c1ae976efd08e4eb8e24d8
Restyled.io <commits@restyled.io>
2023-11-19 07:43:45 +0000

    Restyled by prettier

--- client/app/sign-in/page.js
+++ client/app/sign-in/page.js
@@ -1,49 +1,48 @@
 "use client";
-import {SignIn} from "@clerk/nextjs";
-import {Box, CircularProgress} from "@mui/material";
-import {useAuth} from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
+import { Box, CircularProgress } from "@mui/material";
+import { useAuth } from "@clerk/nextjs";
 import Image from "next/image";
-import {useTheme} from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
 
 export default function Page() {
-  const {isLoaded} = useAuth();
+  const { isLoaded } = useAuth();
   const theme = useTheme();
 
   return (
     <Box
-  key = "signup"
-  width = "100vw"
-  height = "100vh"
-  display = "flex"
-  flexDirection = "row"
-  justifyContent = "center"
+      key="signup"
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="row"
+      justifyContent="center"
       alignItems="center"
     >
       {!isLoaded ? (
         <CircularProgress />
       ) : (
         <Box
-      initial = {
-        { opacity: 0 }
-      } animate = {
-        { opacity: 1 }
-      } exit = {
-        { opacity: 0 }
-      } transition = {
-        { duration: 0.5 }
-      } width = "100vw"
-      height = "100vh"
-      padding = {0} margin = {0} backgroundColor = "background.inverted"
-      display = "flex"
-      flexDirection = "row"
-      justifyContent = "space-between"
-      alignItems =
-          "center" >
-          <div style =
-               {{ width: "50%", height: "100%", position: "relative" }}>< Image
-      priority = {true} src = "/spiderweb.png"
-      alt = "spiderweb"
-      layout = "fill"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          width="100vw"
+          height="100vh"
+          padding={0}
+          margin={0}
+          backgroundColor="background.inverted"
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <div style={{ width: "50%", height: "100%", position: "relative" }}>
+            <Image
+              priority={true}
+              src="/spiderweb.png"
+              alt="spiderweb"
+              layout="fill"
               objectFit="cover"
             />
           </div>
--- client/app/sign-up/page.js
+++ client/app/sign-up/page.js
@@ -1,67 +1,67 @@
 "use client";
-import {SignUp} from "@clerk/nextjs";
-import {Box, CircularProgress} from "@mui/material";
-import {useAuth} from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
+import { Box, CircularProgress } from "@mui/material";
+import { useAuth } from "@clerk/nextjs";
 import Image from "next/image";
 import Stack from "@mui/material/Stack";
-import {useTheme} from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
 
 export default function Page() {
-  const {isLoaded} = useAuth();
+  const { isLoaded } = useAuth();
   const theme = useTheme();
 
   return (
     <Box
-  key = "signup"
-  width = "100vw"
-  height = "100vh"
-  display = "flex"
-  flexDirection = "row"
-  justifyContent = "center"
-  alignItems = "center"
+      key="signup"
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="row"
+      justifyContent="center"
+      alignItems="center"
       backgroundColor="background.inverted"
     >
       {!isLoaded ? (
         <CircularProgress />
       ) : (
         <Stack
-      initial = {
-        { opacity: 0 }
-      } animate = {
-        { opacity: 1 }
-      } exit = {
-        { opacity: 0 }
-      } transition = {
-        { duration: 0.5 }
-      } width = "100vw"
-      height = "100vh"
-      padding = {0} margin = {0} backgroundColor = "theme.palette.red.main"
-      direction = "row"
-      justifyContent = "space-between"
-      alignItems =
-          "center" > {" "} {/* <Box width="50%" height="100%"> */}<div style = {
-            { width: "50%", height: "100%", position: "relative" }
-          }>< Image
-      priority = {true} src = "/spiderweb.png"
-      alt = "spiderweb"
-      layout = "fill"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          width="100vw"
+          height="100vh"
+          padding={0}
+          margin={0}
+          backgroundColor="theme.palette.red.main"
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          {" "}
+          {/* <Box width="50%" height="100%"> */}
+          <div style={{ width: "50%", height: "100%", position: "relative" }}>
+            <Image
+              priority={true}
+              src="/spiderweb.png"
+              alt="spiderweb"
+              layout="fill"
               objectFit="cover"
             />
           </div>
           {/* </Box> */}
           <Box
-              backgroundColor = "theme.palette.red.main"
-              width = "50%"
-              height = "100%"
-              display = "flex"
-              alignItems = "center"
+            backgroundColor="theme.palette.red.main"
+            width="50%"
+            height="100%"
+            display="flex"
+            alignItems="center"
             justifyContent="center"
           >
             <SignUp />
           </Box>
         </Stack>
-      )
-}
-< /Box>
+      )}
+    </Box>
   );
 }
