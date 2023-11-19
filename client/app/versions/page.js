0dc9ae864789244db6d9dcfb745969de40af8204
Restyled.io <commits@restyled.io>
2023-11-19 02:17:44 +0000

    Restyled by prettier

--- client/app/versions/page.js
+++ client/app/versions/page.js
@@ -1,13 +1,15 @@
-'use client';
-import {Box, Pagination, Typography} from '@mui/material';
-import {useState, useEffect} from 'react';
-import axios from 'axios';
+"use client";
+import { Box, Pagination, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Page() {
   const [version, setVersion] = useState(1);
   const [versionsList, setVersionsList] = useState([]);
 
-  useEffect(() => { fetchVersions(); }, []);
+  useEffect(() => {
+    fetchVersions();
+  }, []);
 
   const fetchVersions = async () => {
     try {
@@ -22,25 +24,32 @@ export default function Page() {
       // console.log(response);
       // setVersionsList(response.data);
     } catch (error) {
-      console.error('Error fetching versions:', error);
+      console.error("Error fetching versions:", error);
     }
   };
 
-  const handleChange = (event, value) => { setVersion(value); };
+  const handleChange = (event, value) => {
+    setVersion(value);
+  };
 
-    return (
-        <Box display="flex" width="100vw" height="100vh" alignItems="center" justifyContent="center">
-            <Box
-    width = "50vw"
-    height = "100vh"
-    display = "flex"
-    flexDirection = "column"
-            alignItems="center"
-            >
-                <Pagination count={10} page={version} onChange={
-    handleChange} />
-                <Typography variant='h2'>Version {version}</Typography>
-            </Box>
-        </Box>
-    );
+  return (
+    <Box
+      display="flex"
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Box
+        width="50vw"
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <Pagination count={10} page={version} onChange={handleChange} />
+        <Typography variant="h2">Version {version}</Typography>
+      </Box>
+    </Box>
+  );
 }
